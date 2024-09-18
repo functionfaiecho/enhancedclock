6c450fca6d4fad54f3fa4bb7bb5a78e18e687c0e
Faizah <168539677+functionfaiecho@users.noreply.github.com>
2024-09-18 13:44:58 +1200

    Changed background colour of the whole application itself.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 pb-20">
+    <div className="min-h-screen p-8 pb-20 bg-background">
       {/* Add your content here */}
     </div>
   );
