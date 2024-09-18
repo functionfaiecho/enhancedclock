adc7e553538e4ec16c46d85e61e7e22eaa145a3d
Faizah <168539677+functionfaiecho@users.noreply.github.com>
2024-09-18 20:12:43 +1200

    Add page title

---
+++ app/(components)/ClockTitle.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const ClockTitle: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-textColour">Static Clock</h1>
+      <h3 className="text-sm text-textColour font-mono">CS 210 - Programming Languages Enhancement</h3>
+    </div>
+  );
+};
+
+export default ClockTitle;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Clock from "./(components)/Clock";
+import ClockTitle from "./(components)/ClockTitle";
 
 export default function Home() {
   return (
     <div className="min-h-screen p-8 pb-20 bg-background">
+      <ClockTitle />
       <Clock />
     </div>
   );
